11d21c73f68c0149931875a87d85eed931ba6d20
Pradeep <pradeepgundluru077@gmail.com>
2019-07-04 22:31:49 +0530

    Add files via upload
    added details assignment

---
+++ Details.java
@@ -0,0 +1,14 @@
+class Details
+{
+	public static void main(String[] args)
+	{
+		String name="Pradeep";
+		int age=21;
+		double height=5.6;
+		char last_Letter_Of_Name='p';
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(last_Letter_Of_Name);
+	}
+}
