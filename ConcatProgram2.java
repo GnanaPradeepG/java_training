e3dabe2a97f67c08f5dadc638b440ad6cdf12eea
Pradeep <pradeepgundluru077@gmail.com>
2019-07-06 14:44:03 +0530

    ConcatPrograms

---
+++ ConcatProgram2.java
@@ -0,0 +1,9 @@
+class ConcatProgram2
+{
+	public static void main(String[] args)
+	{
+		double j=8.6;
+		System.out.println(j);
+		System.out.println("j= "+j);
+	}
+}
---
+++ ConcatProgram3.java
@@ -0,0 +1,9 @@
+class ConcatProgram3
+{
+	public static void main(String[] args)
+	{
+		String name = "raja";
+		System.out.println(name);
+		System.out.println(name+ " is a developer");
+	}
+}
