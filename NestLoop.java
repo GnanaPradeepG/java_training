6331580bd5d943482097b78cbeb02b493ca98508
Pradeep <pradeepgundluru077@gmail.com>
2019-07-11 01:07:06 +0530

    PatternPrograms

---
+++ NestLoop.java
@@ -0,0 +1,16 @@
+class NestLoop 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Start Excercise");
+			for(int i=1; i<=3; i++)
+			{
+				for(int j=1; j<=3; j++)
+				{
+					System.out.println("Pushup");
+				}
+				System.out.println("Take 1 min rest");
+			}
+				System.out.println("Stop Excercise");
+	}
+}
