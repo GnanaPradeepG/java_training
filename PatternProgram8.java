220cf0ab3766212e3417feaeca730ab6ac045619
Pradeep <pradeepgundluru077@gmail.com>
2019-07-15 23:55:44 +0530

    Pattern Programs_2
    a little deep into patterns

---
+++ PatternProgram8.java
@@ -0,0 +1,19 @@
+class  PatternProgram8
+{
+	public static void main(String[] args) 
+	{
+		int star=0;
+		for(int i=1;i<=7;i++)
+		{
+			star++;
+			if((i%2)!=0)
+			{
+				for(int j=1;j<=star;j++)
+				{	
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+	}
+}
---
+++ PatternProgram8_2.java
@@ -0,0 +1,16 @@
+class  PatternProgram8_2
+{
+	public static void main(String[] args) 
+	{
+		int star=-1;
+		for (int i=1;i<=4;i++)
+		{
+			star+=2;
+			for(int j=1;j<=star;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
